442916a0537bd13752e54cb4db33980e31c70345
anastasiashoyko3-gif <anastasiashoyko3@gmail.com>
2026-07-07 06:33:48 -0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+export default defineConfig({
+  build:{rollupOptions:{input:{
+    main:resolve(__dirname,'index.html'),
+    admin:resolve(__dirname,'admin.html'),
+    game:resolve(__dirname,'game.html'),
+    viewer:resolve(__dirname,'viewer.html')
+  }}}
+});
