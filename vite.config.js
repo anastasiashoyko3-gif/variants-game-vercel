a5dd7ffc534bd42497a5a38e97bacc6766b77eba
anastasiashoyko3-gif <anastasiashoyko3@gmail.com>
2026-07-07 06:07:56 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-export default defineConfig({
-  build:{rollupOptions:{input:{
-    main:resolve(__dirname,'index.html'),
-    admin:resolve(__dirname,'admin.html'),
-    game:resolve(__dirname,'game.html')
-  }}}
-});
