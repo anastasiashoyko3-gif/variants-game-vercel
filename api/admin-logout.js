21bf22d67da0acf6474ee08a913d047a8d47cbad
anastasiashoyko3-gif <anastasiashoyko3@gmail.com>
2026-07-07 07:13:07 -0500

    Delete api/admin-logout.js

--- api/admin-logout.js
+++
@@ -1,6 +0,0 @@
-const COOKIE_NAME = 'variants_admin_session';
-
-export default function handler(req,res){
-  res.setHeader('Set-Cookie', `${COOKIE_NAME}=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0`);
-  return res.status(200).json({ok:true});
-}
